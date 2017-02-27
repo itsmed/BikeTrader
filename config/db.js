6e85939ebb1cafcb48d090345e1387a21169236c
DejonGill <dejonjgill@gmail.com>
2017-02-27 14:19:33 -0800

    moved database info to environmental variables

--- config/db.js
+++ config/db.js
@@ -1,10 +1,6 @@
 // Maybe add more config files and add them in server.js
 var mongoose = require('mongoose');
 
-module.exports = {
-  url: 'mongodb://tritiatimmins:Harley*8@ds019658.mlab.com:19658/biketrader'
-};
-
 var dbURL = process.env.NODE_ENV === 'production' ? process.env.DB_URL : process.env.DB_URL_DEV;
 
 module.exports = mongoose.connect(dbURL);
